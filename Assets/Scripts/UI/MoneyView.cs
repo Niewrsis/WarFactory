c4c639f9dbc4214f1a6307cdd706e181cd0c682a
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-04-07 14:22:41 +0300

    UI Tree

--- Assets/Scripts/UI/MoneyView.cs
+++ Assets/Scripts/UI/MoneyView.cs
@@ -5,6 +5,9 @@ public class MoneyView : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _moneyField;
     [SerializeField] private TextMeshProUGUI _moneyFieldTree;
+    [SerializeField] private TextMeshProUGUI _moneyFieldEconomy;
+    [SerializeField] private TextMeshProUGUI _moneyFieldMillitary;
+    [SerializeField] private TextMeshProUGUI _moneyFieldTech;
     private ResourceBank _resourceBank;
 
     public void Initialize(ResourceBank resourceBank)
@@ -16,5 +19,8 @@ public class MoneyView : MonoBehaviour
     {
         _moneyField.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
         _moneyFieldTree.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
+        _moneyFieldEconomy.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
+        _moneyFieldMillitary.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
+        _moneyFieldTech.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
     }
 }
--- Assets/Scripts/UI/SetActiveMainScere.cs
+++ Assets/Scripts/UI/SetActiveMainScere.cs
@@ -4,10 +4,17 @@ public class SetActiveMainScere : MonoBehaviour
 {
     [SerializeField] private GameObject _mainScreen;
     [SerializeField] private GameObject _skilTreeScren;
+    [SerializeField] private GameObject _economyTreeScreen;
+    [SerializeField] private GameObject _millitaryTreeScreen;
+    [SerializeField] private GameObject _techTreeScreen;
 
     private void Awake()
     {
-        _skilTreeScren.SetActive(false);
         _mainScreen.SetActive(true);
+
+        _skilTreeScren.SetActive(false);
+        _economyTreeScreen.SetActive(false);
+        _techTreeScreen.SetActive(false);
+        _millitaryTreeScreen.SetActive(false);
     }
 }
