d485a1ba9ceb3e63221bfb5343037a6d3161ac5b
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-04-07 15:30:00 +0300

    Generator 1 Interval Upgrader

--- Assets/Scripts/GeneratorsSystem/Generator1.cs
+++ Assets/Scripts/GeneratorsSystem/Generator1.cs
@@ -7,16 +7,21 @@ public class Generator1 : MonoBehaviour
     private ResourceBank _resourceBank;
 
     [SerializeField] private Button _buyingGen1;
+    [SerializeField] private Button _buyingGen1Interval;
 
-    private int _gen1Cost = 200;
-    private long _gen1Income = 1;
-    private float _gen1Interval = 1.0f;
+    [SerializeField] private int _gen1Cost = 200;
+    [SerializeField] private long _gen1Income = 1;
+    [SerializeField] private float _gen1Interval = 1.0f;
+
+    [SerializeField] private int _gen1IntervalCost = 200;
+    [SerializeField] private float _gen1IntervalMinus = 0.05f;
 
     private bool _isBought = false;
 
     private void Start()
     {
         _buyingGen1.onClick.AddListener(BuyGen);
+        _buyingGen1Interval.onClick.AddListener(IntervalUpgrader);
     }
 
     IEnumerator Income()
@@ -40,6 +45,18 @@ public class Generator1 : MonoBehaviour
         {
             _resourceBank.Money -= _gen1Cost;
             _gen1Income++;
+        }
+    }
+
+    private void IntervalUpgrader()
+    {
+        if (_gen1Interval <= 0.1)
+            return;
+
+        if (_resourceBank.Money >= _gen1IntervalCost)
+        {
+            _resourceBank.Money -= _gen1IntervalCost;
+            _gen1Interval -= _gen1IntervalMinus;
         }
     }
 
