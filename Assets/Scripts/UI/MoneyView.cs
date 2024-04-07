69606ea0c82a479d16179357bd2275fad3065e2b
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-04-07 14:43:53 +0300

    Economy UI Generators

--- Assets/Scripts/UI/MoneyView.cs
+++ Assets/Scripts/UI/MoneyView.cs
@@ -5,7 +5,13 @@ public class MoneyView : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _moneyField;
     [SerializeField] private TextMeshProUGUI _moneyFieldTree;
+
     [SerializeField] private TextMeshProUGUI _moneyFieldEconomy;
+    [SerializeField] private TextMeshProUGUI _gen1FieldEconomy;
+    [SerializeField] private TextMeshProUGUI _gen2FieldEconomy;
+    [SerializeField] private TextMeshProUGUI _gen3FieldEconomy;
+    [SerializeField] private TextMeshProUGUI _gen4FieldEconomy;
+
     [SerializeField] private TextMeshProUGUI _moneyFieldMillitary;
     [SerializeField] private TextMeshProUGUI _moneyFieldTech;
     private ResourceBank _resourceBank;
@@ -19,7 +25,13 @@ public class MoneyView : MonoBehaviour
     {
         _moneyField.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
         _moneyFieldTree.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
+
         _moneyFieldEconomy.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
+        _gen1FieldEconomy.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
+        _gen2FieldEconomy.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
+        _gen3FieldEconomy.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
+        _gen4FieldEconomy.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
+
         _moneyFieldMillitary.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
         _moneyFieldTech.text = StringNumbersFormatter.FormatNumber(_resourceBank.Money);
     }
--- Assets/Scripts/UI/SetActiveMainScere.cs
+++ Assets/Scripts/UI/SetActiveMainScere.cs
@@ -4,7 +4,13 @@ public class SetActiveMainScere : MonoBehaviour
 {
     [SerializeField] private GameObject _mainScreen;
     [SerializeField] private GameObject _skilTreeScren;
+
     [SerializeField] private GameObject _economyTreeScreen;
+    [SerializeField] private GameObject _gen1EconomyScreen;
+    [SerializeField] private GameObject _gen2EconomyScreen;
+    [SerializeField] private GameObject _gen3EconomyScreen;
+    [SerializeField] private GameObject _gen4EconomyScreen;
+
     [SerializeField] private GameObject _millitaryTreeScreen;
     [SerializeField] private GameObject _techTreeScreen;
 
@@ -13,7 +19,13 @@ public class SetActiveMainScere : MonoBehaviour
         _mainScreen.SetActive(true);
 
         _skilTreeScren.SetActive(false);
+
         _economyTreeScreen.SetActive(false);
+        _gen1EconomyScreen.SetActive(false);
+        _gen2EconomyScreen.SetActive(false);
+        _gen3EconomyScreen.SetActive(false);
+        _gen4EconomyScreen.SetActive(false);
+
         _techTreeScreen.SetActive(false);
         _millitaryTreeScreen.SetActive(false);
     }
