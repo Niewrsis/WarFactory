37b10ec7dd9deda91bfa32bd05bb2ef7bb2f09b2
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-05-16 14:59:04 +0300

    Level Loader main scene fix

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,7 +5,13 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
     [SerializeField] private Animator _transition;
+    [SerializeField] private GameObject _wideUp, _wideDown;
     private float _transitionTime = 1f;
+    private void Awake()
+    {
+        _wideUp.SetActive(true);
+        _wideDown.SetActive(true);
+    }
     public IEnumerator LoadLevel(int index)
     {
         Debug.Log("1234");
