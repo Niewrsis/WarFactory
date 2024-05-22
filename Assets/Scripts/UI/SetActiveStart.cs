4ae7257ff663ca0b4f11eec9de52db301404491b
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-05-22 18:44:50 +0300

    Quit script

--- Assets/Scripts/UI/SetActiveStart.cs
+++ Assets/Scripts/UI/SetActiveStart.cs
@@ -3,20 +3,14 @@ using UnityEngine;
 public class SetActiveStart : MonoBehaviour
 {
     [SerializeField] private GameObject _firstFloor;
-    [SerializeField] private GameObject _secondFloor;
-    [SerializeField] private GameObject _thirdFloor;
-    [SerializeField] private GameObject _fourthFloor;
-    [SerializeField] private GameObject _fifthFloor;
+    [SerializeField] private GameObject[] _floors;
 
     [SerializeField] private GameObject[] _settings;
     private void Start()
     {
         _firstFloor.SetActive(true);
-        _secondFloor.SetActive(false);
-        _thirdFloor.SetActive(false);
-        _fourthFloor.SetActive(false);
-        _fifthFloor.SetActive(false);
 
+        for (int i = 0; i < _floors.Length; i++) _floors[i].SetActive(false);
         for (int i = 0;  i < _settings.Length; i++) _settings[i].SetActive(false);
     }
 }
