5aa46398a1b4ce1ac58a6ba820e086e013d4f095
Niewrsis <154369161+Niewrsis@users.noreply.github.com>
2024-05-31 12:54:01 +0300

    Music

--- Assets/Scripts/Audio/MusicSettings.cs
+++ Assets/Scripts/Audio/MusicSettings.cs
@@ -7,6 +7,12 @@ public class MusicSettings : MonoBehaviour
     [SerializeField] private float _musicVolume;
     [SerializeField] private Slider _volumeSlider;
     [SerializeField] private TextMeshProUGUI _volumePercentText;
+    private GameObject _audioSource;
+    private void Awake()
+    {
+        _audioSource = GameObject.FindGameObjectWithTag("Music");
+        DontDestroyOnLoad(_audioSource);
+    }
     private void Start()
     {
         _musicVolume = PlayerPrefs.GetFloat("MasterVolume", _musicVolume);
@@ -17,6 +23,7 @@ public class MusicSettings : MonoBehaviour
     {
         PlayerPrefs.SetFloat("MasterVolume", _musicVolume);
         _musicVolume = _volumeSlider.value;
+        _audioSource.GetComponent<AudioSource>().volume = _musicVolume / 100;
         _volumePercentText.text = $"{Mathf.RoundToInt(_musicVolume)}%";
     }
     private void OnApplicationQuit() => PlayerPrefs.SetFloat("MasterVolume", 100f);
